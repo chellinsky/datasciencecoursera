1276cfc91e9cce94d4a33c9402729eade56c2d35
Andrew Chellinsky <andrew@chellinsky.com>
2016-06-30 13:27:02 -0500

    Finishing R Programming course

--- HospitalData/rankall.R
+++ HospitalData/rankall.R
@@ -1,7 +1,10 @@
-rankall <- function(outcome, num = 1) {
+rankall <- function(outcome, num = "best") {
         #Read outcome data
     outcomefile <- read.csv("outcome-of-care-measures.csv", colClasses = "character", na.strings=c("Not Available","NA"))
     ailments <- c("heart attack", "heart failure", "pneumonia")
+
+    #fix best num
+    if (num == "best") num <- 1
 
     #Check that state and outcome are valid
     if (!any(ailments == outcome))  {
@@ -23,14 +26,29 @@ rankall <- function(outcome, num = 1) {
     correctCol <- paste("Hospital.30.Day.Death..Mortality..Rates.from.", outcome, sep="")
 
     #Remove NAs
-    returnablefile <- outcomefile[!is.na(returnablefile[, correctCol]), ]
+    returnablefile <- outcomefile[!is.na(outcomefile[, correctCol]), ]
 
     #Convert column to numeric
     returnablefile[, correctCol] <- as.numeric(returnablefile[, correctCol])
 
-    #order the data
-    ordereddata <- returnablefile[order(returnablefile[, correctCol], returnablefile[, "Hospital.Name"]), ]
+    statesplit <- split(returnablefile, returnablefile$State)
 
-    rankhospital <- ordereddata[, "Hospital.Name"]
+    rankhospital <- NULL
 
+    for (astate in statesplit) {
+
+        #order the data
+        ordereddata <- astate[order(astate[, correctCol], astate[, "Hospital.Name"]), ]
+
+        num2 <- num
+
+        #account for "worst" as a num
+        if (num == "worst") num2 <- nrow(astate)
+
+        thisrow <- c(ordereddata[1, "State"], ordereddata[num2, "Hospital.Name"], nrow(astate), num2)
+
+        rankhospital <- rbind(rankhospital, thisrow)
+    }
+
+    rankhospital
 }
